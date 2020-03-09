df1072f8d400c9b82eda3a117dc9c0d7969b4512
Sara Falamaki <sarafalamaki-github@falamaki.id.au>
2020-03-09 20:32:33 +1100

    made it work with api server on localhost :-)

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export OPENFISCA_URL=http://localhost:5000
+export GITHUB_URL=https://github.com/Openfisca-NSW/openfisca_nsw_API
+export OPENFISCA_GIT_CLONE_URL=https://github.com/Openfisca-NSW/openfisca_nsw_API
+export LANG=en
+export RAILS_ENV=development
+export RAILS_LOG_TO_STDOUT=true
+export RAILS_SERVE_STATIC_FILES=true
+export COUNTRY_NAME=nsw_API
